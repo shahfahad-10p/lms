16be9326f62be2ca9c4aa9e1cd719797f3050373
Shah Fahad <shah.fahad@10pearls.local>
2020-05-11 02:23:12 +0500

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+  },
+
+  transpileDependencies: ['vuetify'],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/page.github.io/'
+    : '/'
+}
